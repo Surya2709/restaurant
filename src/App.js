4c07cdf40cc3e8b1f2f6bd2d50ace127f369e2a6
Surya Ramachandran <surya@ranktechnologies.com>
2021-06-11 16:27:02 +0530

    modified:   package-lock.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hello raahul
+          Edit <code>src/App.js</code> hello raahul and surya 
         </p>
         <a
           className="App-link"
