42c03ba44b159818ba3f056c5d89e7be0dcadf14
Raahulgoogul <raahulgoogul@gmail.com>
2021-06-11 20:45:44 +0530

    modified:   package-lock.json 	modified:   src/App.js 	deleted:    src/login.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reports from './pages/Reports';
-import Products from './pages/Products';
+import React from'react'
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
-        </Switch>
-      </Router>
-    </>
-  );
+const App = () => {
+return(
+  <div>
+    App
+  </div>
+)
 }
-
-export default App;
