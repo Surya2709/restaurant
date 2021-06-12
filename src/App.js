92fe0aea676b5f344f38037db13da7e142665c90
Surya Ramachandran <surya@ranktechnologies.com>
2021-06-12 22:34:24 +0530

    deleted:    src/App.css 	modified:   src/App.js 	modified:   src/index.js 	deleted:    src/main.js 	deleted:    src/pages/Home.js deleted:    src/pages/Products.js 	deleted:    src/pages/Reports.js 	modified:   src/pages/login.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import "./App.css";
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reports from './pages/Reports';
-import Products from './pages/Products';
+import {BrowserRouter, Switch, Route, NavLink} from "react-router-dom";
 import Login from './pages/login';
-
-
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    
-      <Router>
-        <Switch>
-          <Route path='/' exact component={Login} />
-          <Route path='/home' component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
-          
-        </Switch>
-      </Router>
-   
+    <div className="App">
+      <BrowserRouter>
+        
+        <div className="content">
+          <Switch>
+            <Route  exact path="/login" component={Login}  />
+            <Route  exact path="/dashboard" component={Dashboard}  />           
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import './login.css';
 
 function Login(){
 
     return(
-        <div>
-            login page
+        <div class="login_form">
+            <div class="details">
+                <div class="welcome">Welcome</div>
+                    <form action="/" method="post">
+                        <div class="wrap">
+                            <label>Email</label>
+                            <input type="text" class="input"/>
+                            </div>
+                        <div class="wrap">
+                            <label>Password</label>
+                            <input type="password" class="input" data-type="password"/>
+                            </div>
+                        <div class="wrap">
+                            <label>Forgot password?</label>
+                            </div>
+                    <button type="submit" class="button"><h1 class="sign">Sign in!</h1></button>
+                    </form>
+            </div>
+
+
+        <div class="details-two">
+            <img src="" />
+        <h1 class="back">Welcome back!</h1>
+            <p class="log">Log in and use the opportunities offered by our portal.</p>
+            <h2 class="acc">  let's rule the empire  </h2>
+           
         </div>
+        
+        </div>
+    
+
     );
 }
 
