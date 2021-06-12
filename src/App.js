b327d2ffcbc5bec88715a35a4031f7cd6a678047
Surya Ramachandran <surya@ranktechnologies.com>
2021-06-13 00:16:18 +0530

    modified:   src/App.js 	modified:   src/pages/login.css 	modified:   src/pages/login.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route, NavLink} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Login from './pages/login';
 import Dashboard from './pages/Dashboard';
 
@@ -10,7 +10,7 @@ function App() {
         
         <div className="content">
           <Switch>
-            <Route  exact path="/login" component={Login}  />
+            <Route  exact path="/" component={Login}  />
             <Route  exact path="/dashboard" component={Dashboard}  />           
           </Switch>
         </div>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,38 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './login.css';
+import kitchen from '../kitchen.gif';
 
-function Login(){
+const Login = (props) =>{
+
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+
+const [error,setError] = useState(null);
+const [loading,setLoading] = useState(false);
+
+
+const handleLogin =() => {
+    props.history.push('/dashboard')
+}
 
     return(
         <div class="login_form">
             <div class="details">
                 <div class="welcome">Welcome</div>
-                    <form action="/" method="post">
+                    <form >
                         <div class="wrap">
-                            <label>Email</label>
-                            <input type="text" class="input"/>
+                            <label>Username</label>
+                            <input type="text" class="input" value={username} onChange={ e => setUsername(e.target.value)}/>
                             </div>
                         <div class="wrap">
                             <label>Password</label>
-                            <input type="password" class="input" data-type="password"/>
+                            <input type="password" class="input" data-type="password" value={password} onChange={ e => setPassword(e.target.value)}/>
                             </div>
+                            
                         <div class="wrap">
                             <label>Forgot password?</label>
                             </div>
-                    <button type="submit" class="button"><h1 class="sign">Sign in!</h1></button>
+                        <div class="wrap">
+                                {error && <div className="error">{error}</div>}
+                            </div>
+                           
+                    <input type="button" value={loading ? "Loading.." : "Login"} disabled = {loading} onClick={handleLogin} class="button" value="Signin"/>
                     </form>
             </div>
 
-
         <div class="details-two">
-            <img src="" />
-        <h1 class="back">Welcome back!</h1>
-            <p class="log">Log in and use the opportunities offered by our portal.</p>
-            <h2 class="acc">  let's rule the empire  </h2>
-           
+            <img class='kitchen-logo' src={kitchen} />
+        <h1 class="back">Hey Welcome Again!</h1>
+            <p class="log">  Build by Growmore Corps, India </p>
+            <h1 class="acc">Log in into your system</h1>
         </div>
-        
-        </div>
+    </div>
     
 
     );
